e5505b60959f9af4e98a01f5b4081f8cc8cd863e
MrXck <1971877559@qq.com>
2023-01-17 09:36:10 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-view/>
+    <div id="message"></div>
+</template>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+}
+
+body {
+    background-color: white;
+}
+</style>
+<script>
+import socket from '@/utils/Socket'
+
+export default {
+    components: {},
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,158 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('../views/Index')
+  },
+  {
+    path: '/space/:id',
+    name: 'space',
+    component: () => import('../views/UserDetail'),
+    children: [
+      {
+        path: '',
+        name: 'User',
+        component: () => import('../views/User')
+      },
+      {
+        path: 'dynamic',
+        name: 'UserDynamic',
+        component: () => import('../views/UserDynamic')
+      },
+      {
+        path: 'video',
+        name: 'UserVideo',
+        component: () => import('../views/UserVideo')
+      },
+      {
+        path: 'fans',
+        name: 'Fan',
+        component: () => import('../views/Fan'),
+        children: [
+          {
+            path: 'follow',
+            name: 'Follow',
+            component: () => import('../views/Follow')
+          },
+          {
+            path: 'fans',
+            name: 'Fans',
+            component: () => import('../views/Fans')
+          },
+        ]
+      },
+      {
+        path: 'favlist',
+        name: 'FavList',
+        component: () => import('../views/FavList'),
+      },
+    ]
+  },
+  {
+    path: '/dynamic',
+    name: 'DynamicPage',
+    component: () => import('../views/DynamicPage')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import('../views/Search')
+  },
+  {
+    path: '/message',
+    name: 'Message',
+    redirect: '/message/reply',
+    component: () => import('../views/MessagePage'),
+    children: [
+      {
+        path: 'reply',
+        name: 'Reply',
+        component: () => import('../views/MessageReply')
+      },
+      {
+        path: 'love',
+        name: 'Love',
+        component: () => import('../views/ReceiveStar')
+      },
+      {
+        path: 'system',
+        name: 'System',
+        component: () => import('../views/SystemNotice')
+      },
+      {
+        path: 'whisper',
+        name: 'Whisper',
+        component: () => import('../views/MyMessage'),
+        children: [
+          {
+            path: ':id',
+            name: 'ChatWindow',
+            component: () => import('../views/ChatWindow')
+          },
+        ]
+      },
+    ]
+  },
+  {
+    path: '/history',
+    name: 'HistoryPage',
+    component: () => import('../views/HistoryPage')
+  },
+  {
+    path: '/video/:id',
+    name: 'VideoPage',
+    component: () => import('../views/VideoPage')
+  },
+  {
+    path: '/upload',
+    name: 'UploadPage',
+    redirect: '/upload/video',
+    component: () => import('../views/UploadPage'),
+    children: [
+      {
+        path: 'video',
+        name: 'UploadVideo',
+        component: () => import('../views/UploadVideo')
+      },
+      {
+        path: 'text',
+        name: 'UploadText',
+        component: () => import('../views/UploadText')
+      }
+    ]
+  },
+  {
+    path: '/read/:id',
+    name: 'ReadPage',
+    component: () => import('../views/ReadPage')
+  },
+  {
+    path: '/dynamicDetail/:id',
+    name: 'DynamicDetailPage',
+    component: () => import('../views/DynamicDetailPage')
+  },
+  {
+    path: '/watchLater',
+    name: 'WatchLaterPage',
+    component: () => import('../views/WatchLaterPage')
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
